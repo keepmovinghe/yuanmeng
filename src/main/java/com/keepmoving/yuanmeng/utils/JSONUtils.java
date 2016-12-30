b8a4814df12224355782e15317c7ceecd3b763b5
keepmovinghe <chen615550721@163.com>
2016-12-30 11:14:29 +0800

    修改工具类

--- src/main/java/com/keepmoving/yuanmeng/utils/JSONUtils.java
+++ src/main/java/com/keepmoving/yuanmeng/utils/JSONUtils.java
@@ -2,6 +2,7 @@ package com.keepmoving.yuanmeng.utils;
 
 import java.util.Map;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 /**
@@ -38,12 +39,39 @@ public class JSONUtils {
 	}
 
 	/**
-	 * json string 转换为string
+	 * 返回JSONObject
 	 * 
-	 * @param obj
+	 * @param s
 	 * @return
 	 */
-	public static String toJSONString(Object obj) {
-		return JSON2Object.toJSONString(obj);
+	public static JSONObject getJSONObject(String s) {
+		JSONObject jsonObject = JSONObject.fromObject(s);
+		return jsonObject;
+	}
+
+	public static JSONArray getJSONArray(String s, String key) {
+		JSONObject jsonObject = JSONObject.fromObject(s);
+		JSONArray jsonArray = null;
+		if (jsonObject.get(key) instanceof JSONArray) {
+			jsonArray = jsonObject.getJSONArray(key);
+		}
+		return jsonArray;
+	}
+
+	/**
+	 * 从JSONArray中返回map
+	 * 
+	 * @param jsonArray
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> getMapFromJSONArray(JSONArray jsonArray) {
+		Map<String, Object> objectMap = null;
+		if (!jsonArray.isEmpty()) {
+			if (jsonArray.get(0) instanceof Map<?, ?>) {
+				objectMap = (Map<String, Object>) jsonArray.get(0);
+			}
+		}
+		return objectMap;
 	}
 }
